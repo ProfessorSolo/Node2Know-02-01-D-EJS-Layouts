52be3613fd5945101e40d8ae5c8f77dd87ced839
Josh Solomon <josh@solomoncraft.com>
2026-02-08 19:21:41 -0800

    remove unneeded res.sendFile

--- routers/indexRouter.js
+++ routers/indexRouter.js
@@ -3,9 +3,6 @@ const indexRouter = express.Router();
 
 indexRouter.get("/", (req, res) => res.render("index", { title: "Home" }));
 
-// route to demonstrate EJS rendering a static HTML file
-indexRouter.get("/raw-html", (req, res) => res.render("raw-html"));
-
 // route using the sidebar layout
 indexRouter.get("/dashboard", (req, res) => {
   res.render("dashboard", { 
